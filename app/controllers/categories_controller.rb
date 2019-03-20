f02173d2736db859d6858b79010ad5033c2ce9ec
Sean <shonkelli@gmail.com>
2019-03-20 16:07:45 -0400

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,15 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def create
+    category = Category.create(category_params)
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,8 @@ class CommentsController < ApplicationController
   end
 
   private
-
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+
   end
 end
